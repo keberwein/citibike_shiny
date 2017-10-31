60d0805320fbc7503240d9919441c538f5dad899
Keberwein <kristian.eberwein@yahoo.com>
2017-10-31 14:20:47 -0400

    adjust sidebar size

--- test.R
+++ test.R
@@ -10,7 +10,7 @@ stations <- citibike$stationBeanList %>% subset(testStation=="FALSE" & statusKey
 
 ui <- bootstrapPage(
     tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
-    leafletOutput("map", width = "75%", height = "100%"),
+    leafletOutput("map", width = "77%", height = "100%"),
     absolutePanel(top = 10, right = 10,
                   sliderInput("range", "Available Bikes", min(stations$availableBikes), max(stations$availableBikes),
                               value = range(stations$availableBikes), step = 1
